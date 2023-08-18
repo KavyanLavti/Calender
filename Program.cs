573b62cf826b42d0f2fb62517775f6ca209d1f83
KavyanLavti <109773980+KavyanLavti@users.noreply.github.com>
2023-08-18 20:18:34 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace phybox
+{
+    class program
+    {
+        static public void Main()
+        {
+             using(Game game = new Game(800, 600, "learnOpenTK"))
+             {
+                game.Run();
+             }
+        }
+       
+        
+    }
+}
